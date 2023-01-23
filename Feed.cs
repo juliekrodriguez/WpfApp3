16aaf974e127b44ce3774a971304b6758f0a692e
julie <julie.rodriguez1@wsu.edu>
2023-01-22 17:12:42 -0800

    Add project files.

---
+++ Feed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Feed
+    {
+        public string URL { get; }
+
+        public string? name { get; set; }
+
+        public Feed(string url)
+        {
+            this.URL = url;
+        }
+
+        public Feed(string url, string name)
+        {
+            this.URL = url;
+
+            this.name = name;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public List<Feed> Feeds = new List<Feed>();
+
+        public User(string name)
+        {
+            this.Name = name;
+        }
+
+        public bool AddFeed(Feed feed)
+        {
+            if (this.Feeds.Contains(feed))
+            {
+                return false;
+            }
+            else
+            {
+                this.Feeds.Add(feed);
+                return true;
+            }
+        }
+
+    }
+}
